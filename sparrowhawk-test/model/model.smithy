c469296947d5d4f4a571d6fb8b2ab9207f0d19b8
Richard Hernandez <richiehernandez@mac.com>
2026-03-10 12:30:06 -0700

    Make the demo structure more complex

--- sparrowhawk-test/model/model.smithy
+++ sparrowhawk-test/model/model.smithy
@@ -18,4 +18,37 @@ structure DemoInput {
     @idx(1)
     @required
     str: String
+
+    @idx(2)
+    @required
+    f: PrimitiveFloat = 0
+
+    @idx(3)
+    @required
+    d: PrimitiveDouble = 0
+
+    @idx(4)
+    @required
+    i: PrimitiveInteger = 0
+
+    @idx(5)
+    @required
+    bytes: Blob
+
+    @idx(6)
+    nested: NestedStructure
+}
+
+structure NestedStructure {
+    @idx(1)
+    @required
+    innerStr: String
+
+    @idx(2)
+    @required
+    list: VarintList
+}
+
+list VarintList {
+    member: PrimitiveInteger
 }
--- sparrowhawk-test/src/test/java/demo/DemoTest.java
+++ sparrowhawk-test/src/test/java/demo/DemoTest.java
@@ -1,7 +1,11 @@
 package demo;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.HexFormat;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import software.amazon.smithy.java.sparrowhawk.SparrowhawkSerializer;
 import software.amazon.smithy.model.Model;
@@ -20,10 +24,17 @@ class DemoTest {
     public void enhance() {
         var obj = new DemoInput();
         obj.setStr("hello");
+        obj.setD(1.234);
+        obj.setI(-500);
+        obj.setF(Float.MAX_VALUE);
+        obj.setBytes(ByteBuffer.wrap("hello".getBytes(StandardCharsets.UTF_8)));
+        var nested = new NestedStructure();
+        nested.setInnerStr("inner str");
+        nested.setList(List.of(1, 2, 3));
+        obj.setNested(nested);
 
         var ser = new SparrowhawkSerializer(obj.size());
         obj.encodeTo(ser);
-
         System.err.println(HexFormat.of().formatHex(ser.payload()));
         System.err.println();
         new Enhancer(MODEL).enhance(ser.payload(), MODEL.expectShape(ShapeId.from("demo#DemoInput")));
